b3ec389d410c927bcbe415d9dea40fcd14aa3c57
Bernardo Campos <62954018+bernardocamps@users.noreply.github.com>
2021-12-26 22:11:56 -0300

    first commit

---
+++ ACGs/Model/CardContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ACGs.Model
+{
+	public class CardContent
+	{
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? HourRule { get; set; }
+        public int? HourLimit { get; set; }
+        public string? CreditSystem { get; set; }
+    }
+}
+
---
+++ ACGs/Model/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ACGs.Model
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
+
